beac83948215cfaf006e1cc7e378bb3f5888da45
ihwa.choi <ihwa.choi@samsung.com>
2024-05-16 16:56:00 +0900

    [feature] page 38

--- PrimeFactors_Test/test.cpp
+++ PrimeFactors_Test/test.cpp
@@ -3,9 +3,17 @@
 #include "../PrimeFactors/source.cpp"
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
+class PrimeFixture : public testing::Test {
+public:
 	PrimeFactor prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
