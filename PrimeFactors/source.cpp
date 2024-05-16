d37211f1da9c7550511eff9ee02458e29f16d06c
ihwa.choi <ihwa.choi@samsung.com>
2024-05-16 16:43:58 +0900

    [feature] page 34

---
+++ PrimeFactors/source.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+class PrimeFactor {
+public:
+	vector<int> of(int i) {
+		vector<int> primes;
+
+		return primes;
+	}
+};
---
+++ PrimeFactors_Test/test.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include<vector>
+#include "../PrimeFactors/source.cpp"
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
