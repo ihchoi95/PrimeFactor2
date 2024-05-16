bf4fdca93deab00660a5c6352e7605e1470620a9
ihwa.choi <ihwa.choi@samsung.com>
2024-05-16 17:00:47 +0900

    [feature] page 40

--- PrimeFactors/source.cpp
+++ PrimeFactors/source.cpp
@@ -5,10 +5,14 @@ using namespace std;
 class PrimeFactor {
 public:
 	vector<int> of(int number) {
-		vector<int> result;
+		vector<int> result = {};
 
 		if (number == 2) {
 			result.push_back(2);
+		}
+
+		if (number == 3) {
+			result.push_back(3);
 		}
 
 		return result;
